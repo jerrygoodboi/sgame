ef76ad64c274a827a61ede067260d027311b44ec
Jerry Ron Sunny <rabbitron295@gmail.com>
2023-04-04 23:38:15 +0530

    added fall

--- plate.c
+++ plate.c
@@ -10,20 +10,28 @@ void init(){
 		clear();
 	mvprintw(maxy-1,maxx/2,"===");
 	refresh();
+	srand(time(0));
 }
 int main(){
 	init();
-	int x=maxx/2,y=maxy-1,val;
+	int px=maxx/2,py=maxy-1,val,ax=(rand()%maxx),ay=0;
 	while((val=getch())!=27){
-		if(val == right && x != maxx-3 ){
-		x++;
+		clear();
+		mvprintw(ay,ax,"*");
+		ay++;
+		if(val == right && px != maxx-3 ){
+		px++;
 		}
-		else if(val==left && x != 0){
-		x--;
+		else if(val==left && px != 0){
+		px--;
 		}
-	clear();
-	mvprintw(y,x,"===");
+	//clear();
+	mvprintw(py,px,"===");
 	refresh();
 	}
+/*	while(1){
+	mvprintw(ay,ax,"*");
+	refresh
+	}*/
 	endwin();
 }
